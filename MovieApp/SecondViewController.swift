d049d7ad2aa2473ca18a10b129e8e52919827fd0
Sayali Kale <sayalikale2712@gmail.com>
2020-10-22 14:46:11 -0700

    Initial Commit

---
+++ MovieApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MovieApp
+//
+//  Created by sayali on 22/10/20.
+//  Copyright © 2020 sayali. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
